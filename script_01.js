ba67f8f827d8d56bd62d813ddd4ae41fa5856a6e
Dales92 <dlesnik23@gmail.com>
2022-05-12 14:38:23 +0200

    Funktionen/ callee / Parameter

--- script_01.js
+++ script_01.js
@@ -4,3 +4,24 @@
 
 // 1. Kapselung von Codeblöcken
 
+// Funktionsaufruf (call)
+//test();
+
+// Funktionsrumpf (body) | callee
+function test()
+{
+    console.log("Grizabella");
+}
+
+
+/***** FUnktionen 02a ******/ 
+// Parametisierung + Datenübergabe von INNEN 
+
+ausgabeNamen();
+ 
+function ausgabeNamen() {
+    console.log("Hallo Bernhard!");
+    console.log("Hallo Mohanned!");
+    console.log("Hallo Bianca!");
+}
+ 
